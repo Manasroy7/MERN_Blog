b8a7a8628eb53da5f354a21866a8d6d1befdc91f
Manas <manasroy@gmail.com>
2024-05-10 20:58:36 +0530

    Connect to the MongoDb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose
+.connect(process.env.MONGO_URL).then(
+    ()=>{
+        console.log("MongoDB is connected");
+    }
+)
+.catch(
+    (err)=>{
+        console.log(err);
+    }
+)
 
 const app = express();
 
 app.listen(4000, ()=>{
     console.log("Server is running on port 4000!");
 })
+
