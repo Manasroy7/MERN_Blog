75efbd12809694f8911f3ba6de64d897fcde849b
Manas <manasroy@gmail.com>
2024-05-27 21:34:03 +0530

    add redux persist

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -47,7 +47,7 @@ export const signin = async (req, res, next) =>{
         //add cokiee
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {expiresIn: '3d'});
 
-        const { password: pass, ...rest } = validUser._doc;
+        const { password: pass, ...rest } = validUser._doc;  //// remove the password from the form/web page
 
         res.status(200)
         .cookie('access_token', token, {
